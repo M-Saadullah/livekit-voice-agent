5bbc544917eb0443e97b7d5f079f484e2d4592a8
M-Saadullah <saadullahm037@gmail.com>
2025-11-04 19:01:03 +0500

    new changes for deployment on render.com

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3001';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': {}  // prevent undefined reference errors
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: backendUrl,
-        changeOrigin: true,
-      }
-    }
+export default defineConfig(() => {
+  const backendUrl = process.env.VITE_BACKEND_URL || 'http://localhost:3001'
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': {}  // prevent undefined reference errors
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: backendUrl,
+          changeOrigin: true,
+        },
+      },
+    },
   }
 })
